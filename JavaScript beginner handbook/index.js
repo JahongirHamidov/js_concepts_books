f7a23d261243a7b069fba679b747763c88e19d53
Jahongir <hamidovjahongir444@gmail.com>
2021-10-12 20:31:37 +0500

    javaScript handbook added

---
+++ JavaScript beginner handbook/index.js
@@ -0,0 +1,57 @@
+/* 
+                                        JavaScript is a programming language that is:
+
+    *high level: it provides abstractions that allow you to ignore the details of the machine where it's running on. It manages memory 
+        automatically with a garbage collector, so you can focus on the code instead of managing memory like other languages like 
+        C would need, and provides many constructs which allow you to deal with highly powerful variables and objects. 
+
+    *dynamic: opposed to static programming languages, a dynamic language executes at runtime many of the things that a static language 
+        does at compile time. This has pros and cons, and it gives us powerful features like dynamic typing, late binding, reflection, 
+        functional programming, object runtime alteration, closures and much more. Don't worry if those things are unknown to you - you'll 
+        know all of those at the end of the course. 
+
+    *dynamically typed: a variable does not enforce a type. You can reassign5 any type to a variable, for example, assigning an integer 
+        to a variable that holds a string. 
+    
+    *loosely typed: as opposed to strong typing, loosely (or weakly) typed languages do not enforce the type of an object, allowing more 
+        flexibility but denying us type safety and type checking (something that TypeScript - which builds on top of JavaScript - provides) 
+    
+    *interpreted: it's commonly known as an interpreted language, which means that it does not need a compilation stage before a program 
+        can run, as opposed to C, Java or Go for example. In practice, browsers do compile JavaScript before executing it, for performance 
+        reasons, but this is transparent to you: there is no additional step involved. 
+    
+    *multi-paradigm: the language does not enforce any particular programming paradigm, unlike Java for example, which forces the use of 
+        object-oriented programming, or C that forces imperative programming. You can write JavaScript using an object-oriented paradigm, 
+        using prototypes and the new (as of ES6) classes syntax. You can write JavaScript in a functional programming style, with its 
+        first-class functions, or even in an imperative style (C-like)
+*/
+
+
+/*
+                                        Syntax 
+    In this little introduction I want to tell you about 5 concepts: 
+        1) white space 
+        2) case sensitivity 
+        3) literals 
+        4) identifiers 
+        5) comments
+*/
+
+/* 
+                            In JavaScript we have 2 main kinds of types: 
+                                *primitive types and *object types
+
+    1) Primitive types Primitive types are 
+        *numbers 
+        *strings 
+        *booleans 
+        *symbols 
+        
+        And two special types: null and undefined
+
+    2) Object types 
+        Any value that's not of a primitive type (a string, a number, a boolean, null or undefined) is an object. 
+        Object types have properties and also have methods that can act on those properties. 
+*/
+
+console.log(2**2)
---
+++ index.js
@@ -0,0 +1,12 @@
+class MyClass  {
+    constructor(name){
+        this.name = name
+    }
+    hello(){
+        return console.log('hello world')
+    }
+}
+MyClass.name = 'name'
+class myClass1 extends MyClass {
+
+ }
