44249908ebb6be4ce857b509bc0a8e9e173ae660
Jahongir <hamidovjahongir444@gmail.com>
2021-10-21 17:13:59 +0500

    ES5 to ESnext book

---
+++ ES5 to ESnext/index.js
@@ -0,0 +1,12 @@
+
+                        // Template tags
+const Button = styled.button`
+    font-size: 1.5em; background-color: black; color: white; 
+    `
+// mostly used Graphql servers
+
+const query = gql`
+    query{
+        ...
+    }
+`
---
+++ You don't know JS ES6 and above/index.js
@@ -0,0 +1,6 @@
+// Difference between var and let 
+var a = 2;
+{ let a = 3; 
+    console.log( a ); // 3 
+}
+    console.log( a ); // 2
