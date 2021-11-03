1244b134017c45aeb366194b93d469ca6070e8e9
Jahongir <hamidovjahongir444@gmail.com>
2021-11-03 16:29:24 +0500

    regex added

--- ES5 to ESnext/index.js
+++ ES5 to ESnext/index.js
@@ -102,6 +102,103 @@ Object.setPrototypeOf(object, prototype)
     dog.isMammal //true
 
 
-                
+                @ Template tags
+    Tagged templates is one feature that might sound less useful at first for you, but it's actually used by lots of popular 
+libraries around, like Styled Components or Apollo, the GraphQL client/server lib, 
+so it's essential to understand how it works.
+
+    const Button = styled.button` 
+        font-size: 1.5em; 
+        background-color: black; 
+        color: white; 
+    `
+
+    const query = gql`
+        query: {
+            ...
+        }
+    `
 
 
+                @ Spread Properties   ES2018
+    const { first, second, ...others } = { first: 1, second: 2, third: 3, fourth: 4, fifth: 5 }
+        first // 1 
+        second // 2 
+        others // { third: 3, fourth: 4, fifth: 5 }
+
+
+
+
+
+
+
+
+
+
+
+                        REGULAR EXPRESSION IMPROVEMENTS
+    RegExp lookbehind assertions: match a string depending on what precedes it 
+        This is a lookahead: you use ?= to match a string that's followed by a specific substring:
+            /Roger(?=Waters)/ 
+            /Roger(?= Waters)/.test('Roger is my dog') //false 
+            /Roger(?= Waters)/.test('Roger is my dog and Roger Waters is a famous musician') //true
+
+        ?! performs the inverse operation, matching if a string is not followed by a specific substring:
+            /Roger(?!Waters)/ 
+            /Roger(?! Waters)/.test('Roger is my dog') //true 
+            /Roger(?! Waters)/.test('Roger Waters is a famous musician') //false
+
+        Lookaheads use the ?= symbol. They were already available. 
+        Lookbehinds, a new feature, uses ?<= .
+            /(?<=Roger) Waters/ 
+            /(?<=Roger) Waters/.test('Pink Waters is my dog') //false 
+            /(?<=Roger) Waters/.test('Roger is my dog and Roger Waters is a famous musician') //true
+
+        A lookbehind is negated using ?<! :
+            /(?<!Roger) Waters/ 
+            /(?<!Roger) Waters/.test('Pink Waters is my dog') //true 
+            /(?<!Roger) Waters/.test('Roger is my dog and Roger Waters is a famous musician') //false
+        
+        Unicode property escapes \p{…} and \P{…}
+    In a regular expression pattern you can use \d to match any digit, 
+        \s to match any character that's not a white space, \w to match any alphanumeric character, and so on. 
+    This new feature extends this concept to all Unicode characters introducing \p{} and is negation \P{} . 
+    
+    Any unicode character has a set of properties. For example Script determines the language family, 
+        ASCII is a boolean that's true for ASCII characters, and so on. 
+    You can put this property in the graph parentheses, and the regex will check for that to be true:
+
+        /^\p{ASCII}+$/u.test('abc') //✅ 
+        /^\p{ASCII}+$/u.test('ABC@') //✅ 
+        /^\p{ASCII}+$/u.test('ABCݑ ') //❌
+
+    ASCII_Hex_Digit is another boolean property, that checks if the string only contains valid hexadecimal digits:
+        /^\p{ASCII_Hex_Digit}+$/u.test('0123456789ABCDEF') //✅ 
+        /^\p{ASCII_Hex_Digit}+$/u.test('h') //
+
+    There are many other boolean properties, which you just check by adding their name in the 
+        graph parentheses, including Uppercase , Lowercase , White_Space , Alphabetic , Emoji and more
+            /^\p{Lowercase}$/u.test('h') //✅ 
+            /^\p{Uppercase}$/u.test('H') //✅ 
+            /^\p{Emoji}+$/u.test('H') //❌ 
+            /^\p{Emoji}+$/u.test('ݑ ݑ ') //✅
+    
+    In addition to those binary properties, you can check any of the unicode character properties to match a specific value. 
+    In this example, I check if the string is written in the greek or latin alphabet:
+            /^\p{Script=Greek}+$/u.test('ελληνικά') //✅ 
+            /^\p{Script=Latin}+$/u.test('hey') //✅
+
+    
+                    @ Named capturing groups
+    In ES2018 a capturing group can be assigned to a name, rather than just being assigned a slot in the result array:
+            const re = /(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})/ 
+            const result = re.exec('2015-01-02') 
+            
+            // result.groups.year === '2015'; 
+            // result.groups.month === '01'; 
+            // result.groups.day === '02';
+
+    The s flag for regular expressions The s flag, short for single line, causes the .
+        to match new line characters as well. Without it, the dot matches regular characters but not the new line
+            /hi.welcome/.test('hi\nwelcome') // false 
+            /hi.welcome/s.test('hi\nwelcome') // true
